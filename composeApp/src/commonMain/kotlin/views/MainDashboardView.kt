b1a488012a94e4d28bfec570f04a2a9541222786
Nabeel KM <nabeelkahlil403@gmail.com>
2024-05-12 00:01:41 +0700

    adjust theme

--- composeApp/src/commonMain/kotlin/views/MainDashboardView.kt
+++ composeApp/src/commonMain/kotlin/views/MainDashboardView.kt
@@ -1,5 +1,6 @@
 package views
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -7,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -30,60 +32,62 @@ fun MainDashboardView(
 ) {
     val lang = "en"
     val userName = "Nabeel"
-    
-    Box(
-        modifier = Modifier
-            .padding(16.dp)
-            .pointerInput(Unit) {
-                detectDragGestures { change, dragAmount ->
-                    change.consume()
-                    val swipeLeft = dragAmount.x < 0
-                    val enoughSwipe = dragAmount.x.absoluteValue > 30.dp.toPx()
 
-                    if (enoughSwipe) {
-                        if (swipeLeft) {
-                            navigateToTodoList()
-                        } else {
-                            // swipe right go to other app
+    Surface {
+        Box(
+            modifier = Modifier
+                .padding(16.dp)
+                .pointerInput(Unit) {
+                    detectDragGestures { change, dragAmount ->
+                        change.consume()
+                        val swipeLeft = dragAmount.x < 0
+                        val enoughSwipe = dragAmount.x.absoluteValue > 30.dp.toPx()
+
+                        if (enoughSwipe) {
+                            if (swipeLeft) {
+                                navigateToTodoList()
+                            } else {
+                                // swipe right go to other app
+                            }
                         }
-                    }
 
+                    }
+                }
+        ) {
+            // Main Content
+            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                Column {
+                    Text("Hello,", style = MaterialTheme.typography.headlineSmall)
+                    Text(userName, style = MaterialTheme.typography.headlineLarge)
                 }
             }
-    ) {
-        // Main Content
-        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-            Column {
-                Text("Hello,", style = MaterialTheme.typography.headlineSmall)
-                Text(userName, style = MaterialTheme.typography.headlineLarge)
+
+            // Top Left: Date and Time
+            val now by remember { mutableStateOf(Calendar.getInstance(TimeZone.getDefault())) }
+            val day = DAYS[lang]!![now.get(Calendar.DAY_OF_WEEK)]
+            val month = MONTHS[lang]!![now.get(Calendar.MONTH)]
+
+            val currentDate = "$day, ${now.get(Calendar.DATE)} $month ${now.get(Calendar.YEAR)}"
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.TopStart
+            ) {
+                Text(text = currentDate)
             }
-        }
-
-        // Top Left: Date and Time
-        val now by remember { mutableStateOf(Calendar.getInstance(TimeZone.getDefault())) }
-        val day = DAYS[lang]!![now.get(Calendar.DAY_OF_WEEK)]
-        val month = MONTHS[lang]!![now.get(Calendar.MONTH)]
-
-        val currentDate = "$day, ${now.get(Calendar.DATE)} $month ${now.get(Calendar.YEAR)}"
-        Box(
-            modifier = Modifier.fillMaxSize(),
-            contentAlignment = Alignment.TopStart
-        ) {
-            Text(text = currentDate)
-        }
-        // Top Right:
-        // Bottom Right:
-        Box(
-            modifier = Modifier.fillMaxSize(),
-            contentAlignment = Alignment.BottomEnd
-        ){
-            Button(
-                onClick = navigateToTodoList
+            // Top Right:
+            // Bottom Right:
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.BottomEnd
             ){
-                Text("Todos")
+                Button(
+                    onClick = navigateToTodoList
+                ){
+                    Text("Todos")
+                }
             }
+            // Bottom Left:
         }
-        // Bottom Left:
     }
 }
 
